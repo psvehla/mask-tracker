c346910807e1a35f29d05245d54a7f57770776e3
peter <psvehla@hotmail.com>
2017-06-29 16:47:27 +0800

    Sorted binding on dropdown.

---
+++ src/app/mask.ts
@@ -0,0 +1,8 @@
+export class Mask {
+    name: string = "Mask 1";
+    yellow: number = 0;
+    orange: number = 0;
+    red: number = 0;
+    purple: number = 0;
+    brown: number = 0;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component }      from '@angular/core';
+import { NavController }  from 'ionic-angular';
+
+import { Mask } from '../../app/mask';
 
 @Component({
   selector: 'page-home',
@@ -8,10 +10,16 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   private deleteDisabled: boolean = true;
+
+  private masks: Mask[] = [
+    {name: "mask 1", yellow: 0, orange: 0, red: 0, purple: 0, brown: 0},
+    {name: "mask 2", yellow: 0, orange: 0, red: 0, purple: 0, brown: 0},
+    {name: "mask 3", yellow: 0, orange: 0, red: 0, purple: 0, brown: 0}
+  ];
+
+  private currentMask: Mask = this.masks[0];
   private remaining: number = 86;
 
   constructor(public navCtrl: NavController) {
-
   }
-
 }
